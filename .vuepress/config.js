eb8a7d2154dc244971af64578bb6401a8284a6c0
yaesey <nnqyq@outlook.com>
2021-07-05 10:09:05 +0800

    初始化

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "title" : "FutureStand",
+    themeConfig : {
+        nav : [
+            {text: 'Home',link: '/'},
+            {
+                text: '指南',link: '/guide/guide',
+                items:[
+                {text: '参与项目指南',link: '/guide/how-to-join'},
+                {text: '成立项目指南',link: ''},
+                {text: '组织工作指南',link: ''},
+                {text: '交流指南',link: ''}
+            ]}
+        ]
+    }
+}
